6b1082c418236245ccb9aab755cf19dac611ccfe
roboter5123 <pascal.roboter@hotmail.com>
2022-12-07 20:18:33 +0100

    Added comments. For documentation.

--- main.ts
+++ main.ts
@@ -3,10 +3,25 @@ import express from "npm:express@^4.18";
 import getUser from "./db.ts"
 const app = express();
 
+  /**Base route.
+   * 
+   * Examples:
+   * localhost:3000
+   * localhost:3000/
+   */
 app.get("/", function (req: any, res: { send: (arg0: string) => void; }) {
   res.send("Hello World, HALLO PASCAL");
 });
 
+
+  /**
+   * Route for getting a whole user.
+   * 
+   * Examples:
+   * localhost:3000/get/user/1 
+   * localhost:3000/get/user/2 
+   * localhost:3000/get/user/3 
+   */
 app.get("/get/user/:id", async function (req: any, res: { send: (arg0: string) => void; }) {
 
   const id:number = req.params.id;

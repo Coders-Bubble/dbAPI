7b330636dfb69c70da6cceb3ec1bfc88dee2db7f
roboter5123 <pascal.roboter@hotmail.com>
2022-12-07 20:11:50 +0100

    Added a HTTP route for /get/user/:id. That uses the getUserMethod from db.ts to return a whole user as a json

--- main.ts
+++ main.ts
@@ -1,9 +1,17 @@
 // main.ts
 import express from "npm:express@^4.18";
+import getUser from "./db.ts"
 const app = express();
 
 app.get("/", function (req: any, res: { send: (arg0: string) => void; }) {
   res.send("Hello World, HALLO PASCAL");
+});
+
+app.get("/get/user/:id", async function (req: any, res: { send: (arg0: string) => void; }) {
+
+  const id:number = req.params.id;
+  const user = await getUser(id)
+  res.send(user);
 });
 
 app.listen(3000);

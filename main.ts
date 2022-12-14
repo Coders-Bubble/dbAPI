2ea1896d4847c6556da02df4f660ed91dbd57fc6
roboter5123 <Pascal.roboter@hotmail.com>
2022-12-14 12:58:11 +0100

    made it so the params in the url are now given with ?param

--- main.ts
+++ main.ts
@@ -18,14 +18,15 @@ app.get("/", function (_req: any, res: { send: (arg0: string) => void; }) {
   /**Route for getting a whole user.
    * 
    * Examples:
-   * localhost:3000/get/user/1 
-   * localhost:3000/get/user/2 
-   * localhost:3000/get/user/3 
+   * localhost:3000/get/user?id=1 
+   * localhost:3000/get/user?id=2
+   * localhost:3000/get/user?id=3
    */
 
-app.get("/get/user/:id", async function (req: any, res: { send: (arg0: string) => void; }) {
+app.get("/get/user", async function (req: any, res: { send: (arg0: string) => void; }) {
 
-  const id:number = req.params.id;
+  const id:number = req.query.id;
+  console.log("getting user " + id)
   const user = await getUser(id)
   res.send(user);
 });
@@ -33,14 +34,14 @@ app.get("/get/user/:id", async function (req: any, res: { send: (arg0: string) =
 /**Route for comparing a given Password to one in the db.
    * 
    * Examples:
-   * localhost:3000/get/1/123
-   * localhost:3000/get/1/321
+   * localhost:3000/get/?id=1&?password=123
+   * localhost:3000/get/?id=1&?password=321
    */
 
  app.get("/get/:id/:password", async function (req: any, res: { send: (arg0: string) => void; }) {
 
-  const id:number = req.params.id;
-  const passwordToTest:string = req.params.password
+  const id:number = req.query.id;
+  const passwordToTest:string = req.query.password
   res.send(await comparePassword(id,passwordToTest));
 });
 
